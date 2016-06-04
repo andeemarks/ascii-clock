6f1ab8ff4308faaf7897a7216815e49e1bb38905
Andy Marks <amarks@thoughtworks.com>
2016-06-04 20:27:26 +1000

    Now able to handle stacked h and m

--- src/ascii_clock/core.clj
+++ src/ascii_clock/core.clj
@@ -36,17 +36,22 @@
     (replace-first row #"o" token)
     (clojure.string/reverse (replace-first row #"o" token))))
 
-(defn update-clock-for-minutes [clock {row :row position :position}]
-  (update-in clock [(- row 1)] update-row position "m"))
+(defn update-clock-for-segment [clock {row :row position :position} segment]
+  (update-in clock [(- row 1)] update-row position segment))
 
-(defn update-clock-for-hours [clock {row :row position :position}]
-  (update-in clock [(- row 1)] update-row position "h"))
+(defn update-clock-for-minutes [clock coords]
+  (update-clock-for-segment clock coords "m"))
+
+(defn update-clock-for-hours [clock coords]
+  (update-clock-for-segment clock coords "h"))
 
 (defn format-clock [{hours :hours minutes :minutes}]
   (let [minute-update-coords (hour-to-location (map-minute (round-minutes minutes)))
         hour-update-coords (hour-to-location hours)]
-        (update-clock-for-minutes
-          (update-clock-for-hours raw-clock hour-update-coords) minute-update-coords)))
+        (if (= minute-update-coords hour-update-coords)
+          (update-clock-for-segment raw-clock minute-update-coords "x")
+          (update-clock-for-minutes
+            (update-clock-for-hours raw-clock hour-update-coords) minute-update-coords))))
 
 (defn -main [& args]
   (dorun (map println (format-clock {:hours 12 :minutes 19}))))
