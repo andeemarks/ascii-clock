82b7b1b75571920bc8d4ad1cded8332c23410881
Andy Marks <amarks@thoughtworks.com>
2016-06-05 21:18:32 +1000

    Version updates to modern clojure and midje

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ascii-clock "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main "ascii-clock.core"
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                    :plugins [[lein-midje "3.2"]]}})
